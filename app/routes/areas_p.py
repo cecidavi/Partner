7b528338357a324cfb5f99ef139bc2e5f0640d8e
cecidavi <cecireicito_@hotmail.com>
2025-05-29 12:32:50 -0600

    agregue los html faltantes

--- app/routes/areas_p.py
+++ app/routes/areas_p.py
@@ -1,4 +1,5 @@
 # app/routes/areas_p.py
+
 from flask import Blueprint, render_template, redirect, url_for, flash
 from app.models.sugerencia import Sugerencia
 from app.forms.sugerencia_form import SugerenciaForm
@@ -10,6 +11,7 @@ areas_p_bp = Blueprint(
     url_prefix='/areas'
 )
 
+# Sugerencias (ya lo tenías)
 @areas_p_bp.route('/sugerencias', methods=['GET', 'POST'])
 def sugerencias():
     form = SugerenciaForm()
@@ -23,3 +25,40 @@ def sugerencias():
         flash('¡Gracias por tu sugerencia!', 'success')
         return redirect(url_for('areas_p.sugerencias'))
     return render_template('areas_p/sugerencias.html', form=form)
+
+# Ahora añade las rutas para cada área:
+@areas_p_bp.route('/certificaciones')
+def certificaciones():
+    return render_template('areas_p/certificaciones.html')
+
+@areas_p_bp.route('/telecomunicaciones')
+def telecomunicaciones():
+    return render_template('areas_p/telecomunicaciones.html')
+
+@areas_p_bp.route('/redes-electricas')
+def redes_electricas():
+    return render_template('areas_p/redes_electricas.html')
+
+@areas_p_bp.route('/circuito-cerrado-tv')
+def circuito_cerrado_tv():
+    return render_template('areas_p/circuito_cerrado_tv.html')
+
+@areas_p_bp.route('/corriente-regulada')
+def corriente_regulada():
+    return render_template('areas_p/corriente_regulada.html')
+
+@areas_p_bp.route('/data-centers')
+def data_centers():
+    return render_template('areas_p/data_centers.html')
+
+@areas_p_bp.route('/fibra-optica')
+def fibra_optica():
+    return render_template('areas_p/fibra_optica.html')
+
+@areas_p_bp.route('/cables-estructurados')
+def cables_estructurados():
+    return render_template('areas_p/cables_estructurados.html')
+
+@areas_p_bp.route('/polizas')
+def polizas():
+    return render_template('areas_p/polizas.html')
