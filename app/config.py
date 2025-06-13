a2cac4cc8246fa24bb140a07f2bbb8652275afcf
cecidavi <cecireicito_@hotmail.com>
2025-06-12 20:45:09 -0600

    ajustar DATABASE_URL para pymysql

--- app/config.py
+++ app/config.py
@@ -1,13 +1,17 @@
 import os
 
 class Config:
-    # Usa DATABASE_URL de entorno si existe, si no usa el valor local
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
+    raw_url = os.environ.get(
         "DATABASE_URL",
         "mysql+pymysql://cecilio:ceci1282@localhost/parnet_db"
     )
+
+    # Asegura compatibilidad con SQLAlchemy usando pymysql
+    if raw_url.startswith("mysql://"):
+        raw_url = raw_url.replace("mysql://", "mysql+pymysql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = raw_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "1234")
-
     RECAPTCHA_PUBLIC_KEY  = os.environ.get("RECAPTCHA_PUBLIC_KEY", "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI")
     RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY", "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe")
