f73dbb6da38788475153799751256af99b0ef70e
cecidavi <cecireicito_@hotmail.com>
2025-06-12 20:11:22 -0600

    produccion base de datos local/global

--- app/config.py
+++ app/config.py
@@ -1,7 +1,13 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://cecilio:ceci1282@localhost/parnet_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '1234'
+import os
 
-    RECAPTCHA_PUBLIC_KEY  = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
-    RECAPTCHA_PRIVATE_KEY = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+class Config:
+    # Usa DATABASE_URL de entorno si existe, si no usa el valor local
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "mysql+pymysql://cecilio:ceci1282@localhost/parnet_db"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY", "1234")
+
+    RECAPTCHA_PUBLIC_KEY  = os.environ.get("RECAPTCHA_PUBLIC_KEY", "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI")
+    RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY", "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe")
